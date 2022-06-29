4b5d32cb9f6540305f3f76978dd0d8fb884385b2
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-06-29 12:52:34 -0700

    switched to tailwindclassnames

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -7,22 +7,24 @@ import {
   Button,
   Pressable,
 } from 'react-native';
-import tailwind from 'tailwind-rn';
 
-const buttonStyle = 'm-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20';
+import tw from 'twrnc';
+
+const buttonStyle =
+  'm-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20 shadow-lg';
 
 export default function SignIn() {
   return (
     <View>
-      <Text>Email:</Text>
-      <TextInput style={tailwind('border border-gray-400')}></TextInput>
+      <Text style={tw`shadow-xl`}>Email:</Text>
+      <TextInput style={tw`border border-gray-400`}></TextInput>
       <Text>Password:</Text>
-      <TextInput style={tailwind('border border-gray-400')}></TextInput>
-      <Pressable style={tailwind(buttonStyle)}>
-        <Text style={tailwind('font-bold')}>Sign in!!</Text>
+      <TextInput style={tw`border border-gray-400`}></TextInput>
+      <Pressable style={tw`${buttonStyle}`}>
+        <Text style={tw`font-bold`}>Sign in!!</Text>
       </Pressable>
-      <Pressable style={tailwind(buttonStyle)}>
-        <Text style={tailwind('font-bold')}>Sign up!!</Text>
+      <Pressable style={tw`${buttonStyle}`}>
+        <Text style={tw`font-bold`}>Sign up!!</Text>
       </Pressable>
     </View>
   );
--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -1,9 +1,31 @@
-import { View, Text, Button, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Pressable,
+} from 'react-native';
+
+import tw from 'twrnc';
+
+const buttonStyle =
+  'm-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20 shadow-lg';
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>This is the SignUp view</Text>
+    <View style={tw`border`}>
+      <Text style={tw`shadow-xl`}>Email:</Text>
+      <TextInput style={tw`border border-gray-400`}></TextInput>
+      <Text>Password:</Text>
+      <TextInput style={tw`border border-gray-400`}></TextInput>
+      <Pressable style={tw`${buttonStyle}`}>
+        <Text style={tw`font-bold`}>Sign in!!</Text>
+      </Pressable>
+      <Pressable style={tw`${buttonStyle}`}>
+        <Text style={tw`font-bold`}>Sign up!!</Text>
+      </Pressable>
     </View>
   );
 }
