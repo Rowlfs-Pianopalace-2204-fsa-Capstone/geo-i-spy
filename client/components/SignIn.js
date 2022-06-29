8963eeaa0936d43d66faa177a5b34f2000d07402
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-06-29 12:33:47 -0700

    small changes

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -9,7 +9,8 @@ import {
 } from 'react-native';
 import tailwind from 'tailwind-rn';
 
-const buttonStyle = 'm-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20';
+const buttonStyle =
+  'm-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20 shadow';
 
 export default function SignIn() {
   return (
