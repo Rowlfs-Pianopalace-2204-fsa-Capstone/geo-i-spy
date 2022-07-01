b038f1fe67692ff65aad9b413b9a25b1a2e24a64
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-07-01 09:56:31 -0700

    vision integration

--- client/components/CameraComponent.js
+++ client/components/CameraComponent.js
@@ -2,15 +2,72 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import tw from 'twrnc';
+import * as MediaLibrary from 'expo-media-library';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { StackActions } from '@react-navigation/native';
+import * as FileSystem from 'expo-file-system';
 
 export default function CameraComponent({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(CameraType.back);
   const [recording, setRecording] = useState(false);
   const [camera, setCamera] = useState(null);
+  const [uploading, setUpload] = useState(false);
+  const [image, setImage] = useState(null);
+
+  const submitToGoogle = async (image) => {
+    try {
+      setUpload(true);
+      const encoded = await FileSystem.readAsStringAsync(image, {
+        encoding: 'base64',
+      });
+      // const fs = require('fs');
+      // let imageFile = fs.readFileSync(image);
+      // let encoded = Buffer.from(imageFile).toString('base64');
+
+      let body = JSON.stringify({
+        requests: [
+          {
+            features: [
+              { type: 'LABEL_DETECTION', maxResults: 3 },
+              // { type: 'LANDMARK_DETECTION', maxResults: 5 },
+              // { type: 'FACE_DETECTION', maxResults: 5 },
+              // { type: 'LOGO_DETECTION', maxResults: 5 },
+              // { type: 'TEXT_DETECTION', maxResults: 5 },
+              // { type: 'DOCUMENT_TEXT_DETECTION', maxResults: 5 },
+              // { type: 'SAFE_SEARCH_DETECTION', maxResults: 5 },
+              // { type: 'IMAGE_PROPERTIES', maxResults: 5 },
+              // { type: 'CROP_HINTS', maxResults: 5 },
+              // { type: 'WEB_DETECTION', maxResults: 5 },
+            ],
+            image: {
+              content: encoded,
+            },
+          },
+        ],
+      });
+      let response = await fetch(
+        'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA7XC9GtbutkELajb80zbyoOCJfraTFPgg',
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: body,
+        }
+      );
+      let responseJson = await response.json();
+      console.log(responseJson);
+      // this.setState({
+      //   googleResponse: responseJson,
+      //   uploading: false
+      // });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -19,6 +76,10 @@ export default function CameraComponent({ navigation }) {
     })();
   }, []);
 
+  useEffect(() => {
+    console.log('permission', hasPermission);
+  }, [hasPermission]);
+
   const handleRecord = async () => {
     if (recording) {
       await camera.stopRecording();
@@ -26,8 +87,16 @@ export default function CameraComponent({ navigation }) {
       setRecording(false);
     } else if (!recording) {
       setRecording(true);
-      const recording = await camera.recordAsync();
+
+      const recording = await camera.takePictureAsync();
       console.log(recording);
+      submitToGoogle(recording.uri);
+
+      // try {
+      //   await MediaLibrary.saveToLibraryAsync(recording.uri);
+      // } catch (err) {
+      //   console.log(err);
+      // }
     }
   };
 
