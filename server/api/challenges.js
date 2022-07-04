1cff26d071f4157c7728785a1e9c9f8dde15c2fe
Havin Mickel <12345678900qw@gmail.com>
2022-07-04 06:02:35 -0700

    Set up basic challenge routes

--- server/api/challenges.js
+++ server/api/challenges.js
@@ -10,10 +10,41 @@ module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await Challenge.findAll({
-      where: { userId: req.body.id },
+    const challenges = await Challenge.findAll();
+    res.json(challenges);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const challenge = await Challenge.findByPk(req.params.id);
+    res.json(challenge);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    await Challenge.create({
+      name: req.body.name,
+      difficulty: req.body.difficulty,
+      score: req.body.score,
+      description: req.body.description,
     });
-    res.json(users);
+    res.sendStatus(200);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const challenge = await Challenge.findByPk(req.params.id);
+    challenge.destroy();
+    res.sendStatus(200);
   } catch (err) {
     next(err);
   }
