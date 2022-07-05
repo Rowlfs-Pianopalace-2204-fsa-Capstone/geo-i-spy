42417013b1276c0fb91cadd6a57755b1362a7a37
Havin Mickel <12345678900qw@gmail.com>
2022-07-05 14:02:28 -0700

    Plugged in heroku backend

--- client/Thunks/Challenges.js
+++ client/Thunks/Challenges.js
@@ -1,7 +1,7 @@
 /** @format */
 
 export const apiGetAllChallenges = async () => {
-  const response = await fetch('http://localhost:8080/api/challenges', {
+  const response = await fetch('https://geoispy.herokuapp.com/api/challenges', {
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
@@ -13,22 +13,27 @@ export const apiGetAllChallenges = async () => {
 };
 
 export const apiGetSingleChallenge = async (id) => {
-  const response = await fetch(`/api/challenges/${id}`);
+  const response = await fetch(
+    `https://geoispy.herokuapp.com/api/challenges/${id}`
+  );
   const data = await response.json();
   return data;
 };
 
 export const apiDeleteChallenge = async (id) => {
-  const response = await fetch(`/api/challenges/${id}`, {
-    method: 'DELETE',
-    headers: null,
-  });
+  const response = await fetch(
+    `https://geoispy.herokuapp.com/api/challenges/${id}`,
+    {
+      method: 'DELETE',
+      headers: null,
+    }
+  );
   const data = await response.json();
   return data;
 };
 
 export const apiPostChallenge = async (challenge) => {
-  const response = await fetch('/api/challenges', {
+  const response = await fetch('https://geoispy.herokuapp.com/api/challenges', {
     method: 'POST',
     headers: null,
     body: JSON.stringify(challenge),
@@ -38,11 +43,14 @@ export const apiPostChallenge = async (challenge) => {
 };
 
 export const apiUpdateChallenge = async (challenge) => {
-  const response = await fetch(`/api/challenges/${challenge.id}`, {
-    method: 'PUT',
-    headers: null,
-    body: JSON.stringify(challenge),
-  });
+  const response = await fetch(
+    `https://geoispy.herokuapp.com/api/challenges/${challenge.id}`,
+    {
+      method: 'PUT',
+      headers: null,
+      body: JSON.stringify(challenge),
+    }
+  );
   const data = await response.json();
   return data;
 };
