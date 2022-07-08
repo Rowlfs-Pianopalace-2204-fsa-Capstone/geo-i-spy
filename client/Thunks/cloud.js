937c3a27c7888d7b7fc80e4fa8c561894603e8e4
Havin Mickel <12345678900qw@gmail.com>
2022-07-08 12:20:40 -0700

    changes

--- client/Thunks/cloud.js
+++ client/Thunks/cloud.js
@@ -1,14 +1,15 @@
 /** @format */
+import * as SecureStore from 'expo-secure-store';
 
 export const pictureToCloud = async (base64EncodedImage) => {
-  console.log(base64EncodedImage);
   try {
+    const token = await SecureStore.getItemAsync('token');
     const reponse = await fetch(
-      `https://geoispy.herokuapp.com/api/achievements
+      `https://geoispy.herokuapp.com/api/achievements/4
   `,
       {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', authorization: token },
         body: JSON.stringify({ data: base64EncodedImage }),
       }
     );
