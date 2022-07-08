c5fcb40847aceda65c6a87ae0e927872971c3b06
RickyTicky <Rickyabrooks92@gmail.com>
2022-07-08 15:23:30 -0400

    Added alert box for wrong username or password

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -10,21 +10,22 @@ import {
   TouchableOpacity,
   ImageBackground,
   ScrollView,
-} from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { apiAuthLogin } from '../Thunks/Auth';
-import { GlobalDataContext } from '../Context';
-import { GlobalIsSignedContext } from '../Context';
-import tw from 'twrnc';
-import toast from '../helpers/toast';
-import * as SecureStore from 'expo-secure-store';
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { apiAuthLogin } from "../Thunks/Auth";
+import { GlobalDataContext } from "../Context";
+import { GlobalIsSignedContext } from "../Context";
+import tw from "twrnc";
+import toast from "../helpers/toast";
+import * as SecureStore from "expo-secure-store";
 
 const buttonStyle =
-  'm-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20 shadow-lg';
+  "m-1 p-2 bg-blue-400 rounded-lg items-center mr-20 ml-20 shadow-lg";
 
 export default function SignIn({ navigation }) {
-  const [username, setUsername] = useState('cody');
-  const [password, setPassword] = useState('123');
+  const [username, setUsername] = useState("cody");
+  const [password, setPassword] = useState("123");
   const { authData, setAuthData } = React.useContext(GlobalDataContext);
   const { setIsSigned } = React.useContext(GlobalIsSignedContext);
 
@@ -37,25 +38,26 @@ export default function SignIn({ navigation }) {
       setAuthData(user);
       setIsSigned(true);
     } else if (user === undefined) {
-      console.log('testttttt');
-      toast.danger({ message: 'Invalid username or password' });
+      Alert.alert("Alert", "Username or Password is not valid", [
+        { text: "Ok" },
+      ]);
     }
   };
 
   const signUp = () => {
-    navigation.navigate('SignUp');
+    navigation.navigate("SignUp");
   };
   return (
     <ImageBackground
       source={require(`../../public/Bridge.jpeg`)}
-      resizeMode='cover'
+      resizeMode="cover"
       style={tw`flex-1 justify-center`}
     >
       <View style={tw`flex-1 my-20 pl-6 pr-6`}>
         <ScrollView>
           <View style={tw`flex-3 justify-center text-center`}>
             <Text style={tw`font-bold text-4xl bg-gray-100/40 `}>
-              {' '}
+              {" "}
               Welcome to
             </Text>
             <Text style={tw`font-bold text-4xl bg-gray-100/40`}>Geo-I-Spy</Text>
@@ -64,13 +66,13 @@ export default function SignIn({ navigation }) {
             <View style={tw`bg-gray-200/50 p-4`}>
               <Text style={tw`shadow-xl font-bold`}>Username:</Text>
               <TextInput
-                placeholder='username'
+                placeholder="username"
                 onChangeText={(username) => setUsername(username)}
                 style={tw`border border-gray-500 bg-gray-200/75 pl-2`}
               ></TextInput>
               <Text style={tw`shadow-xl font-bold`}>Password:</Text>
               <TextInput
-                placeholder='password'
+                placeholder="password"
                 secureTextEntry={true}
                 onChangeText={(password) => setPassword(password)}
                 style={tw`border border-gray-500  bg-gray-200/75 pl-2`}
