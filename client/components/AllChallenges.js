6e6167676e56756d342b35f4949a21efeadaa80a
Havin Mickel <12345678900qw@gmail.com>
2022-07-11 10:33:04 -0700

    Added filters for all challenges

--- client/components/AllChallenges.js
+++ client/components/AllChallenges.js
@@ -12,37 +12,106 @@ const textStyle = `font-bold`;
 
 import tw from 'twrnc';
 import { GlobalDataContext } from '../Context';
-export const mapArray = (arr, navigation) => {
+export const mapArray = (
+  arr,
+  navigation,
+  setSingleChallengeData,
+  difficulty
+) => {
   return arr.map((ele) => {
-    const { setSingleChallengeData } = React.useContext(GlobalDataContext);
     let color = 'blue';
     if (ele.users) {
       color = 'green';
     } else {
       color = 'blue';
     }
-    return (
-      <TouchableOpacity
-        onPress={() => [
-          setSingleChallengeData(ele),
-          navigation.navigate('SingleChallenge'),
-        ]}
-        key={ele.id}
-        style={tw`border bg-${color}-400 p-6`}
-      >
-        <Text style={tw`${textStyle}`}>{ele.name}</Text>
-        <Text style={tw`${textStyle}`}>{ele.difficulty}</Text>
-        <Text style={tw`${textStyle}`}>{ele.score}</Text>
-      </TouchableOpacity>
-    );
+    if (difficulty === ele.difficulty) {
+      return (
+        <TouchableOpacity
+          onPress={() => [
+            setSingleChallengeData(ele),
+            navigation.navigate('SingleChallenge'),
+          ]}
+          key={ele.id}
+          style={tw`border bg-${color}-400 p-6`}
+        >
+          <Text style={tw`${textStyle}`}>{ele.name}</Text>
+          <Text style={tw`${textStyle}`}>{ele.difficulty}</Text>
+          <Text style={tw`${textStyle}`}>{ele.score}</Text>
+        </TouchableOpacity>
+      );
+    }
   });
 };
 
 export default function AllChallenges({ navigation }) {
-  const { achievements } = React.useContext(GlobalDataContext);
+  const [handleToggle, setHandleToggle] = useState({});
+  const { achievements, setSingleChallengeData } =
+    React.useContext(GlobalDataContext);
+  useEffect(() => {
+    setHandleToggle({
+      hard: false,
+      Medium: false,
+      easy: false,
+    });
+  }, []);
   return (
     <ScrollView style={tw`flex-1 pt-12 px-6`}>
-      {mapArray(achievements, navigation)}
+      <TouchableOpacity
+        style={tw`border bg-blue-400 p-6`}
+        onPress={() => [
+          setHandleToggle({
+            ...handleToggle,
+            hard: !handleToggle.hard,
+          }),
+        ]}
+      >
+        {}
+        <Text>Toggle Hard</Text>
+      </TouchableOpacity>
+
+      {handleToggle.hard ? (
+        mapArray(achievements, navigation, setSingleChallengeData, 'hard')
+      ) : (
+        <></>
+      )}
+
+      <TouchableOpacity
+        style={tw`border bg-blue-400 p-6`}
+        onPress={() => [
+          setHandleToggle({
+            ...handleToggle,
+            medium: !handleToggle.medium,
+          }),
+          console.log(handleToggle),
+        ]}
+      >
+        {}
+        <Text>Toggle Medium</Text>
+      </TouchableOpacity>
+      {handleToggle.medium ? (
+        mapArray(achievements, navigation, setSingleChallengeData, 'medium')
+      ) : (
+        <></>
+      )}
+      <TouchableOpacity
+        style={tw`border bg-blue-400 p-6`}
+        onPress={() => [
+          setHandleToggle({
+            ...handleToggle,
+            easy: !handleToggle.easy,
+          }),
+          console.log(handleToggle),
+        ]}
+      >
+        {}
+        <Text>Toggle easy</Text>
+      </TouchableOpacity>
+      {handleToggle.easy ? (
+        mapArray(achievements, navigation, setSingleChallengeData, 'easy')
+      ) : (
+        <></>
+      )}
       <Text style={tw`h-20`}></Text>
     </ScrollView>
   );
