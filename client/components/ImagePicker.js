619fb0e3f7f09c9fab48d4b06dc53857d48fea31
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-07-11 14:25:30 -0700

    add profile pic

--- client/components/ImagePicker.js
+++ client/components/ImagePicker.js
@@ -2,24 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform, TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as ImagePicker from 'expo-image-picker';
+import { changeProfilePic } from '../Thunks/cloud';
 import tw from 'twrnc';
+import { GlobalDataContext } from '../Context';
 
 export default function ImagePickerComponent() {
+  const { authData, setAuthData } = React.useContext(GlobalDataContext);
+
   const [image, setImage] = useState(null);
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
+      base64: true,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
 
-    console.log(result);
+    //console.log(result);
 
     if (!result.cancelled) {
       setImage(result.uri);
+
+      const newUrl = await changeProfilePic(result.base64);
+
+      const newAuth = { ...authData, img_url: newUrl };
+      setAuthData(newAuth);
     }
   };
 
