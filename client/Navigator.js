f0fec027464ac367831a44a02fb1256d09b3254f
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-07-12 14:59:36 -0700

    redid nav

--- client/Navigator.js
+++ client/Navigator.js
@@ -11,35 +11,67 @@ import {
   Button,
   Pressable,
   ImageBackground,
-} from "react-native";
-import React, { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { GlobalIsSignedContext } from "./Context";
-import tw from "twrnc";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import HomePage from "./components/HomePage";
-import CameraComponent from "./components/CameraComponent";
-import ProfileNavigate from "./components/ProfileNavigate";
+} from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { GlobalIsSignedContext } from './Context';
+import tw from 'twrnc';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import HomePage from './components/HomePage';
+import CameraComponent from './components/CameraComponent';
+import ProfileNavigate from './components/ProfileNavigate';
 
-import AllChallenges from "./components/AllChallenges";
-import SingleChallenge from "./components/SingleChallenge";
+import AllChallenges from './components/AllChallenges';
+import SingleChallenge from './components/SingleChallenge';
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
-const CamNav = () => {
+const HomeNav = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="CameraView"
-        component={CameraComponent}
-        options={{ headerShown: false }}
+    <Tab.Navigator
+      initialRouteName='Home'
+      activeColor='#e91e63'
+      labelStyle={{ fontSize: 12 }}
+      animationEnabled={true}
+      barStyle={{ backgroundColor: 'blue' }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={HomePage}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='home' color={color} size={26} />
+          ),
+        }}
       />
-    </Stack.Navigator>
+      <Tab.Screen
+        name='Challenges'
+        component={AllChallenges}
+        options={{
+          tabBarLabel: 'Challenges',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='compass' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name='account' color={color} size={26} />
+          ),
+        }}
+      >
+        {(props) => <ProfileNavigate {...props} />}
+      </Tab.Screen>
+    </Tab.Navigator>
   );
 };
 
@@ -48,15 +80,16 @@ export default function Navigator() {
   return !isSigned ? (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="SignIn" options={{ headerShown: false }}>
+        <Stack.Screen name='SignIn' options={{ headerShown: false }}>
           {(props) => <SignIn {...props} />}
         </Stack.Screen>
         <Stack.Screen
-          name="SignUp"
+          name='SignUp'
           options={{
             headerShown: true,
+
             headerStyle: {
-              backgroundColor: "lightgray",
+              backgroundColor: 'lightgray',
             },
           }}
         >
@@ -66,69 +99,19 @@ export default function Navigator() {
     </NavigationContainer>
   ) : (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        activeColor="#e91e63"
-        labelStyle={{ fontSize: 12 }}
-        animationEnabled={true}
-        style={{ backgroundColor: "tomato" }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomePage}
-          options={{
-            tabBarLabel: "Home",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
-          }}
+      <Stack.Navigator>
+        <Stack.Screen
+          name='HomeNav'
+          options={{ headerShown: false }}
+          component={HomeNav}
         />
-        <Tab.Screen
-          name="Camera"
-          component={CamNav}
-          options={{
-            tabBarLabel: "Camera",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="camera" color={color} size={26} />
-            ),
-          }}
+        <Stack.Screen
+          name='Camera'
+          component={CameraComponent}
+          options={{ headerShown: false }}
         />
-        <Tab.Screen
-          name="Challenges"
-          component={AllChallenges}
-          options={{
-            tabBarLabel: "Challenges",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="alien" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="SingleChallenge"
-          component={SingleChallenge}
-          options={{
-            tabBarLabel: "Current Challenge",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons
-                name="alien-outline"
-                color={color}
-                size={26}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          options={{
-            tabBarLabel: "Profile",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account" color={color} size={26} />
-            ),
-          }}
-        >
-          {(props) => <ProfileNavigate {...props} />}
-        </Tab.Screen>
-      </Tab.Navigator>
+        <Stack.Screen name='SingleChallenge' component={SingleChallenge} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
