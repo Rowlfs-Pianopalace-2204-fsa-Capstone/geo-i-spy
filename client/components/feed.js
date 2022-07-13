b967bf209fddd9c4d7385e6de2df3f86416f5e00
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-07-13 13:42:39 -0700

    some feed changes

--- client/components/feed.js
+++ client/components/feed.js
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
   View,
   Image,
+  SafeAreaView,
 } from 'react-native';
 const textStyle = `font-bold`;
 import DownArrow from './arrowAnimations/DownArrow';
@@ -29,45 +30,52 @@ export const mapArray = (
   setImg,
   setSingleChallengeData
 ) => {
-  let user;
   return arr.map((ele) => {
-    const LeftContent = () => (
-      <Image source={{ uri: ele.img }} style={tw`h-12 w-12 pl-12`}></Image>
-    );
     return (
-      <Card key={ele.challenge.id} style={tw`mb-8`} resizeMode={'contain'}>
-        <TouchableOpacity onPress={() => [showPublicProfile(ele.id)]}>
-          <Card.Title title='' left={LeftContent} />
-        </TouchableOpacity>
-        <Card.Content>
-          <TouchableOpacity
-            onPress={() => [
-              setSingleChallengeData(ele.challenge),
-              navigation.navigate('SingleChallenge'),
-            ]}
-          >
-            <Title>{ele.challenge.name} challenge!</Title>
-          </TouchableOpacity>
-          <Paragraph>
-            {ele.username} completed the {ele.challenge.name} challenge for a
-            whooping {ele.challenge.score} points!
-          </Paragraph>
-          <Paragraph>
-            {timeSince(new Date(ele.challenge.Achievement.createdAt))} ago
-          </Paragraph>
-          <TouchableOpacity
-            onPress={() => [
-              setImg(ele.challenge.Achievement.img_url),
-              navigation.navigate('ViewPicture'),
-            ]}
-          >
-            <Card.Cover
-              source={{ uri: ele.challenge.Achievement.img_url }}
-              style={tw`h-40`}
+      <View style={tw`pt-5 pb-5`} key={ele.challenge.id}>
+        <Card>
+          <TouchableOpacity onPress={() => [showPublicProfile(ele.id)]}>
+            <Card.Title
+              title={`${ele.challenge.name} challenge!`}
+              left={() => (
+                <Image
+                  source={{ uri: ele.img }}
+                  style={tw`h-12 w-12 pl-12 rounded-full`}
+                />
+              )}
             />
           </TouchableOpacity>
-        </Card.Content>
-      </Card>
+          <Card.Content>
+            <TouchableOpacity
+              onPress={() => [
+                setSingleChallengeData(ele.challenge),
+                navigation.navigate('SingleChallenge'),
+              ]}
+            >
+              <Title></Title>
+            </TouchableOpacity>
+            <Paragraph>
+              {ele.username} completed the {ele.challenge.name} challenge for a
+              whooping {ele.challenge.score} points!
+            </Paragraph>
+
+            <TouchableOpacity
+              onPress={() => [
+                setImg(ele.challenge.Achievement.img_url),
+                navigation.navigate('ViewPicture'),
+              ]}
+            >
+              <Card.Cover
+                source={{ uri: ele.challenge.Achievement.img_url }}
+                style={tw`h-40`}
+              />
+              <Paragraph>
+                {timeSince(new Date(ele.challenge.Achievement.createdAt))} ago
+              </Paragraph>
+            </TouchableOpacity>
+          </Card.Content>
+        </Card>
+      </View>
     );
   });
 };
@@ -94,19 +102,21 @@ export default function Feed({ navigation }) {
     navigation.navigate('PublicProfile');
   };
   return (
-    <ScrollView style={tw`flex-1 pt-12 px-6`}>
-      {feed[0] ? (
-        mapArray(
-          feed,
-          navigation,
-          showPublicProfile,
-          setImg,
-          setSingleChallengeData
-        )
-      ) : (
-        <></>
-      )}
-      <Text style={tw`h-20`}></Text>
-    </ScrollView>
+    <SafeAreaView style={tw`flex-1 items-center justify-center shadow-lg`}>
+      <ScrollView>
+        {feed[0] ? (
+          mapArray(
+            feed,
+            navigation,
+            showPublicProfile,
+            setImg,
+            setSingleChallengeData
+          )
+        ) : (
+          <></>
+        )}
+        <Text style={tw`h-20`}></Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
