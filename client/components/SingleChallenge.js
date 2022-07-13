50429900a8b515dbf199dd588187104a77d96de0
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-07-13 10:46:44 -0700

    home and single changed to card

--- client/components/SingleChallenge.js
+++ client/components/SingleChallenge.js
@@ -8,48 +8,62 @@ import {
   Pressable,
   TouchableOpacity,
   Image,
+  SafeAreaView,
+  ScrollView,
 } from 'react-native';
+
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import tw from 'twrnc';
 import { GlobalDataContext } from '../Context';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function SingleChallenge({ navigation }) {
   const { SingleChallengeData } = React.useContext(GlobalDataContext);
   return (
-    <View style={tw`border-2 border-gray-500 p-25`}>
-      {SingleChallengeData.users ? (
-        <>
-          <Image
-            source={{ uri: SingleChallengeData.users[0].Achievement.img_url }}
-            style={tw`h-50 w-50`}
-          ></Image>
-          <Text>
-            Completed:{' '}
-            {new Date(
-              SingleChallengeData.users[0].Achievement.createdAt + ''
-            ).toDateString()}
-          </Text>
-        </>
-      ) : (
-        <Text>
-          This challenge is to take a picture of a {SingleChallengeData.name}!
-        </Text>
-      )}
-      <Text>Rarity: {SingleChallengeData.difficulty}</Text>
-      <Text>Score: {SingleChallengeData.score}</Text>
-      <Text>Description: {SingleChallengeData.description}</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
-        <View style={tw`bg-blue-500 px-5 py-3 rounded-full`}>
-          {SingleChallengeData.users ? (
-            <Text style={tw`text-white font-semibold text-lg`}>
-              Retake Photo!👋
-            </Text>
-          ) : (
-            <Text style={tw`text-white font-semibold text-lg`}>
-              Complete Challenge👋
-            </Text>
-          )}
+    <SafeAreaView style={tw`flex-1 items-center justify-center shadow-lg`}>
+      <ScrollView>
+        <View style={tw`pt-5 pb-5`}>
+          <Card>
+            <Card.Title
+              title={SingleChallengeData.name}
+              left={() => (
+                <MaterialCommunityIcons
+                  name='camera-iris'
+                  size={40}
+                  color='black'
+                />
+              )}
+            />
+            <Card.Content>
+              <Text>Rarity: {SingleChallengeData.difficulty}</Text>
+              <Text>Score: {SingleChallengeData.score}</Text>
+              <Text>Description: {SingleChallengeData.description}</Text>
+              {SingleChallengeData.users && (
+                <>
+                  <Card.Cover
+                    source={{
+                      uri: SingleChallengeData.users[0].Achievement.img_url,
+                    }}
+                  />
+                  <Text>
+                    Completed:{' '}
+                    {new Date(
+                      SingleChallengeData.users[0].Achievement.createdAt + ''
+                    ).toDateString()}
+                  </Text>
+                </>
+              )}
+            </Card.Content>
+            <Card.Actions>
+              <Button onPress={() => navigation.navigate('Camera')}>
+                {SingleChallengeData.users
+                  ? 'Retake Photo!'
+                  : 'Complete Challenge1'}
+              </Button>
+            </Card.Actions>
+          </Card>
         </View>
-      </TouchableOpacity>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
