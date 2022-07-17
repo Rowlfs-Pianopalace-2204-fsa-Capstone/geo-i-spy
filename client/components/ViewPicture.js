5754556a23ce8c5dc1cb0037f05c6a6c96a4d003
Miguel Cruz <rednu7@yahoo.com>
2022-07-16 21:11:39 -0400

    styling

--- client/components/ViewPicture.js
+++ client/components/ViewPicture.js
@@ -26,13 +26,13 @@ export default function ViewPicture({ navigation }) {
   const { img, singleUser } = React.useContext(GlobalDataContext);
   useEffect(() => {}, [img]);
   return (
-    <Card style={tw` w-full`}>
+    <Card style={tw`flex-1`}>
       <Card.Title title='' />
 
-      <Card.Content style={tw`w-full`}>
+      <Card.Content style={tw`flex-1 p-[1%] items-center `}>
         {/* <Card.ScrollArea>
           <ScrollView contentContainerStyle={{ paddingHorizontal: 24 }}> */}
-        <Card.Cover source={{ uri: img }} style={tw`border-2 w-full`} />
+        <Card.Cover source={{ uri: img }} style={tw`w-90 h-90`} />
         {/* </ScrollView>
         </Card.ScrollArea> */}
       </Card.Content>
